a1359a9802724b8b775cd27185c840682b49b787
liriel <liriel@sekrit.me>
2018-02-27 01:08:32 +0000

    Support year and country in query

--- plugin.py
+++ plugin.py
@@ -15,20 +15,56 @@ from babel.dates import format_timedelta
 from dateutil.parser import parse
 
 def fetch(show=False):
-    if show:
+    if not show:
+        url = 'http://api.tvmaze.com/schedule?country=US'
+    else:
         query_string = '?q=' + quote(show) + '&embed[]=previousepisode&embed[]=nextepisode'
         url = 'http://api.tvmaze.com/singlesearch/shows' + query_string
-    else:
-        url = 'http://api.tvmaze.com/schedule?country=US'
 
     try:
         resp = utils.web.getUrl(url)
+        return  json.loads(resp)
     except utils.web.Error, e:
-        return False
-    
-    data = json.loads(resp)
+        # Check if query is for schedule or a show with no possible modifier
+        if (not show) or len(show.split()) == 1:
+            return False
+        # Check whether using last word in show as year or country returns a valid result
+        else:
+            title = show.rsplit(' ', 1)[0]
+            modifier = show.rsplit(' ', 1)[1]
+   
+            query_string = '?q=' + title
+            url = 'http://api.tvmaze.com/search/shows' + query_string
+            try:
+                resp = utils.web.getUrl(url)
+                data = json.loads(resp)
+              
+                for show2 in data:
+                    id = False
 
-    return data
+                    try: # attributes can be missing so need try statement
+                        if modifier == show2['show']['premiered'][:4]:
+                            id=show2['show']['id']
+                    except:
+                        continue
+
+                    try:
+                        if modifier in show2['show']['network']['country']['code']:
+                            id=show2['show']['id']
+                    except:
+                        continue
+
+                    if id:
+                        query_string = str(id) + '?embed[]=previousepisode&embed[]=nextepisode'
+                        url = 'http://api.tvmaze.com/shows/' + query_string
+                        try:    
+                            resp = utils.web.getUrl(url)                    
+                            return  json.loads(resp)
+                        except:
+                           return false
+            except utils.web.Error, e:
+                return False
+    return False
 
 class tvmaze(callbacks.Plugin):
     threaded=True
@@ -43,7 +79,6 @@ class tvmaze(callbacks.Plugin):
         next = False
         last = False
         
-        print opts
         if opts:
             for (stuff,arg) in opts:
                 if stuff == 'detail':
@@ -67,16 +102,16 @@ class tvmaze(callbacks.Plugin):
             show_title = ircutils.bold('%s (%s)' % (show['name'], premiered[:4]))
 
             if "Ended" in show['status']:
-                show_state = ircutils.mircColor(show['status'],'red')
+                show_state = ircutils.mircColor(show['status'],'red').upper()
             else:
-                show_state = ircutils.mircColor(show['status'],'green')
+                show_state = ircutils.mircColor(show['status'],'green').upper()
             
             if ( '_embedded' in show and 'previousepisode' in show['_embedded']):
                 airtime = parse(show['_embedded']['previousepisode']['airstamp'])
                 timedelta = datetime.datetime.now(tzlocal()) - airtime
                 relative_time = format_timedelta(timedelta,
                         granularity='minutes')
-                last_episode = format('[%s] %s on %s (%s).',
+                last_episode = format('[%s] %s on %s (%s)',
                         ircutils.bold(str(show['_embedded']['previousepisode']['season'])
                             + 'x' +
                             str(show['_embedded']['previousepisode']['number'])),
@@ -91,7 +126,7 @@ class tvmaze(callbacks.Plugin):
                 timedelta = datetime.datetime.now(tzlocal()) - airtime
                 relative_time = format_timedelta(timedelta, granularity='minutes')
 
-                next_episode = format('[%s] %s on %s (%s).',
+                next_episode = format('[%s] %s on %s (%s)',
                         ircutils.bold(str(show['_embedded']['nextepisode']['season'])
                             + 'x' +
                             str(show['_embedded']['nextepisode']['number'])),
@@ -103,22 +138,25 @@ class tvmaze(callbacks.Plugin):
 
             
             if rip:
-                irc.reply(format('%s is %s', show_title, show_state.upper()))
+                irc.reply(format('%s is %s', show_title, show_state))
             elif next:
                 if ('_embedded' in show and 'nextepisode' in show['_embedded']):
                     irc.reply(format('%s next scheduled episode is %s', show_title, next_episode))
                 else: 
                     if "Ended" in show['status']:
-                        irc.reply(format('%s is %s', show_title, show_state.upper()))
+                        irc.reply(format('%s is %s', show_title, show_state))
                     else:
-                        irc.reply(format('%s is %s but does not have a release date for the next episode', show_title, show_state.upper()))
+                        irc.reply(format('%s is %s but does not have a release date for the next episode', show_title, show_state))
             elif last:
                 if ('_embedded' in show and 'previousepisode' in show['_embedded']):
                     irc.reply(format('%s last scheduled episode was %s', show_title, last_episode))
                 else:
                     irc.reply(format('%s has not previously run any episodes', show_title))
             else: 
-                irc.reply(format('%s [%s] %s:%s %s:%s %s', show_title, show_state, ircutils.underline('Next Episode'), next_episode, ircutils.underline('Previous Episode'), last_episode, show['url']))
+                if "Ended" in show['status']:
+                    irc.reply(format('%s %s %s:%s %s', show_title, show_state, ircutils.underline('Previous Episode'), last_episode, show['url']))
+                else:
+                    irc.reply(format('%s %s %s:%s %s:%s %s', show_title, show_state, ircutils.underline('Next Episode'), next_episode, ircutils.underline('Previous Episode'), last_episode, show['url']))
 
         else:
             irc.reply(format('No show found named "%s"', ircutils.bold(tvshow)))
